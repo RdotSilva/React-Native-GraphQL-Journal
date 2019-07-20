0bd1792cc69506b6240ae9f2fe81218e4fa4f7f6
Ryan Silva <RyandotSilva@gmail.com>
2019-07-20 14:00:50 -0400

    Add header styles

---
+++ assets/stylesheets/navigation.js
@@ -0,0 +1,14 @@
+export default {
+	headerStyle: {
+		backgroundColor: "#373142"
+	},
+	headerTintColor: "#fff",
+	headerTitleStyle: {
+		alignSelf: "center",
+		textAlign: "center",
+		justifyContent: "center",
+		flex: 1,
+		fontWeight: "bold",
+		color: "#fff"
+	}
+};

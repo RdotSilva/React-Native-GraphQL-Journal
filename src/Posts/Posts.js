7d047693d7f14ce39239fbde6afed9225df93cab
Ryan Silva <RyandotSilva@gmail.com>
2019-07-21 19:12:59 -0400

    Add onPress to navigate to Post page

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -3,13 +3,15 @@ import { View, Text, FlatList } from "react-native";
 import { compose, graphql } from "react-apollo";
 import { gql } from "apollo-boost";
 
-const Posts = ({ loading, allPosts }) => {
+const Posts = ({ loading, allPosts, navigation }) => {
 	if (loading) return null;
 	return (
 		<View>
 			<FlatList
 				data={allPosts}
-				renderItem={({ item }) => <Text>{item.title}</Text>}
+				renderItem={({ item }) => (
+					<Text onPress={() => navigation.navigate("Post")}>{item.title}</Text>
+				)}
 				keyExtractor={item => item.id}
 			/>
 		</View>

3f963f022adc4888476dde8705e1e21393440dd6
Ryan Silva <RyandotSilva@gmail.com>
2019-07-21 18:59:43 -0400

    Add FlatList to display all posts data

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { compose, graphql } from "react-apollo";
 import { gql } from "apollo-boost";
 
-const Posts = props => {
-	console.log(props);
+const Posts = ({ loading, allPosts }) => {
+	if (loading) return null;
 	return (
 		<View>
-			<Text />
+      <FlatList data={allPosts}
+      
 		</View>
 	);
 };

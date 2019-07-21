8bdaf6f6f8be25288de1ceb000bac0ad0b3d8883
Ryan Silva <RyandotSilva@gmail.com>
2019-07-21 19:34:23 -0400

    Refactor using Query component

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -1,22 +1,28 @@
 import React from "react";
 import { View, Text, FlatList } from "react-native";
-import { compose, graphql } from "react-apollo";
+import { compose, graphql, Query } from "react-apollo";
 import { gql } from "apollo-boost";
 
-const Posts = ({ loading, allPosts, navigation }) => {
-	if (loading) return null;
-	return (
-		<View>
-			<FlatList
-				data={allPosts}
-				renderItem={({ item }) => (
-					<Text onPress={() => navigation.navigate("Post")}>{item.title}</Text>
-				)}
-				keyExtractor={item => item.id}
-			/>
-		</View>
-	);
-};
+const Posts = ({ navigation }) => (
+	<Query query={postsQuery}>
+		{({ loading, data: { allPosts } }) => {
+			if (loading) return <Text>Loading...</Text>;
+			return (
+				<View>
+					<FlatList
+						data={allPosts}
+						renderItem={({ item }) => (
+							<Text onPress={() => navigation.navigate("Post")}>
+								{item.title}
+							</Text>
+						)}
+						keyExtractor={item => item.id}
+					/>
+				</View>
+			);
+		}}
+	</Query>
+);
 
 const postsQuery = gql`
 	{
@@ -27,6 +33,4 @@ const postsQuery = gql`
 	}
 `;
 
-export default graphql(postsQuery, {
-	props: ({ data }) => ({ ...data })
-})(Posts);
+export default Posts;

b2c33c9dca9bba7026fb06f27d588290558d6120
Ryan Silva <RyandotSilva@gmail.com>
2019-07-22 15:07:12 -0400

    Add connection to graphql

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -2,6 +2,9 @@ import React from "react";
 import { View, Text } from "react-native";
 import navStyles from "../../assets/stylesheets/navStyles";
 
+import { compose, graphql, Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
 const Post = props => {
 	console.log(props);
 	return (
@@ -17,4 +20,9 @@ Post.navigationOptions = {
 	...navStyles
 };
 
-export default Post;
+export default graphql(postQuery, {
+	props: ({ data }) => ({ ...data }),
+	options: ({ navigation }) => ({
+		variables: { id: navigation.state.params.id }
+	})
+})(Post);

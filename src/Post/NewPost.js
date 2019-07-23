75925a87bd210c9cf82d5cfe69fa34c2c4f75db2
Ryan Silva <RyandotSilva@gmail.com>
2019-07-23 14:19:06 -0400

    Add newPost function

--- src/Post/NewPost.js
+++ src/Post/NewPost.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
+import PostForm from "./PostForm";
 
 const NewPost = () => {
+	newPost = () => {};
+
 	return (
 		<View>
-			<Text />
+			<PostForm onSubmit={newPost} />
 		</View>
 	);
 };

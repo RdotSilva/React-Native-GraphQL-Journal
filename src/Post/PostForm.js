b9bdb47e1ee258009c7256e29485f5a503bc04db
Ryan Silva <RyandotSilva@gmail.com>
2019-07-23 14:46:31 -0400

    Add useState and set initial state values

--- src/Post/PostForm.js
+++ src/Post/PostForm.js
@@ -2,6 +2,13 @@ import React, { useState, setState } from "react";
 import { View, Text, TextInput, Button } from "react-native";
 
 const PostForm = props => {
+	const [formData, setFormData] = useState({
+		title: "",
+		body: ""
+	});
+
+	const { title, body } = formData;
+
 	submitForm = () => {
 		props.onSubmit({
 			title: title,

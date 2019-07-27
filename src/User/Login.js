491b5ee57da8fc5744a0cecffba9c71183c8a7f6
Ryan Silva <RyandotSilva@gmail.com>
2019-07-27 08:44:41 -0400

    Add Login component

---
+++ src/User/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Login = () => {
+	return (
+		<View>
+			<Text />
+		</View>
+	);
+};
+
+export default Login;

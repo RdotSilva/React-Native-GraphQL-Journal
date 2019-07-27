c9616cd1f70517af68b566bea621853d836af8ab
Ryan Silva <RyandotSilva@gmail.com>
2019-07-27 08:37:07 -0400

    Add UserForm component

---
+++ src/User/UserForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+import { Form, Item, Input, Label } from "native-base";
+
+const UserForm = () => {
+	return (
+		<View>
+			<Text />
+		</View>
+	);
+};
+
+export default UserForm;

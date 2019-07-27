a4b2217d4926bfdd70cc8060418d27517692535e
Ryan Silva <RyandotSilva@gmail.com>
2019-07-27 08:40:07 -0400

    Add useState for form

--- src/User/UserForm.js
+++ src/User/UserForm.js
@@ -3,6 +3,11 @@ import { View, Text, Button } from "react-native";
 import { Form, Item, Input, Label } from "native-base";
 
 const UserForm = () => {
+	const [formData, setFormData] = useState({
+		email: "",
+		password: ""
+	});
+
 	return (
 		<Form>
 			<Item>

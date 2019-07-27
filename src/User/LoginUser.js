65fc6a3d553ffeb37d5a0b1eb5dafb93318ce087
Ryan Silva <RyandotSilva@gmail.com>
2019-07-27 09:04:11 -0400

    Add LoginUser component

---
+++ src/User/LoginUser.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import UserForm from "./UserForm";
+
+const LoginUser = () => {
+	return (
+		<View>
+			<Text>Login</Text>
+			<UserForm type="Login" onSubmit={loginUser} />
+		</View>
+	);
+};
+
+export default LoginUser;

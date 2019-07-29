2afd326702dc87d170d8db27d8a43104ea7f0e5f
Ryan Silva <RyandotSilva@gmail.com>
2019-07-29 15:26:27 -0400

    Add getToken function

--- src/utils/loginUtils.js
+++ src/utils/loginUtils.js
@@ -2,11 +2,19 @@ import { AsyncStorage } from "react-native";
 
 let token;
 
+export const getToken = async () => {
+	if (token) {
+		return Promise.resolve(token);
+	}
+	token = await AsyncStorage.getItem("AUTH_TOKEN");
+	return token;
+};
+
 export const signIn = newToken => {
 	return AsyncStorage.setItem("AUTH_TOKEN", newToken);
 };
 
-export const signOut = newToken => {
+export const signOut = () => {
 	token = undefined;
 	return AsyncStorage.remove("AUTH_TOKEN");
 };

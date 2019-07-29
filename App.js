a1b3fb42e24618f1283b736ad0b2ca90389008d9
Ryan Silva <RyandotSilva@gmail.com>
2019-07-29 16:11:50 -0400

    Update ApolloClient to return token

--- App.js
+++ App.js
@@ -3,28 +3,38 @@ import Navigator from "./src/Navigation/Navigator";
 import { setContext } from "apollo-link-context";
 import { getToken } from "./src/utils/loginUtils";
 
-const authLink = setContext(async (req, { headers }) => {
-	const token = await getToken();
-	return {
-		...headers,
-		headers: {
-			authorization: token ? `Bearer ${token}` : null;
-		}
-	};
-});
-
-const httpLink = new HttpLink({
-	uri: "https://api.graph.cool/simple/v1/cjybv96jy2msk014182vlh1z0"
-})
-
-const link = authLink.concat(httpLink);
-
 // Apollo Import
 import ApolloClient, { HttpLink } from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 
+// const authLink = setContext(async (req, { headers }) => {
+// 	const token = await getToken();
+// 	console.log(token);
+// 	return {
+// 		...headers,
+// 		headers: {
+// 			authorization: token ? `Bearer ${token}` : null
+// 		}
+// 	};
+// });
+
+// const httpLink = new HttpLink({
+// 	uri: "https://api.graph.cool/simple/v1/cjybv96jy2msk014182vlh1z0"
+// });
+
+// const link = authLink.concat(httpLink);
+
 const client = new ApolloClient({
-	uri: link
+	uri: "https://api.graph.cool/simple/v1/cjybv96jy2msk014182vlh1z0",
+	request: async operation => {
+		const token = await getToken();
+		console.log(token);
+		operation.setContext({
+			headers: {
+				authorization: token ? `Bearer ${token}` : null
+			}
+		});
+	}
 });
 
 const App = () => {

c5cd4060be46565c4a92f56f224126b2efea61b1
Ryan Silva <RyandotSilva@gmail.com>
2019-07-29 15:25:02 -0400

    Add signOut function

--- src/utils/loginUtils.js
+++ src/utils/loginUtils.js
@@ -5,3 +5,8 @@ let token;
 export const signIn = newToken => {
 	return AsyncStorage.setItem("AUTH_TOKEN", newToken);
 };
+
+export const signOut = newToken => {
+	token = undefined;
+	return AsyncStorage.remove("AUTH_TOKEN");
+};

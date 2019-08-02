09d49f4ab6aa162b75c740a016623e75b47505e7
Ryan Silva <RyandotSilva@gmail.com>
2019-08-02 17:29:13 -0400

    Remove unused imports

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
 import React from "react";
 import Navigator from "./src/Navigation/Navigator";
 import { getToken } from "./src/utils/loginUtils";
-
-// Apollo Import
-import ApolloClient, { HttpLink } from "apollo-boost";
+import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 
 const client = new ApolloClient({
 	uri: "https://api.graph.cool/simple/v1/cjybv96jy2msk014182vlh1z0",
 	request: async operation => {
 		const token = await getToken();
-		console.log(token);
 		operation.setContext({
 			headers: {
 				authorization: token ? `Bearer ${token}` : null
